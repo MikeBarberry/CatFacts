58620ac703c353483c0a545caa3bd865654950b0
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2023-05-05 01:05:46 -0700

    feat: show origin and description on pygame screen

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import json as pyjs
 from io import BytesIO
 import time
 
-pygame.init()
-displayInfo = pygame.display.Info()
-screen = pygame.display.set_mode(
-    (displayInfo.current_w, displayInfo.current_h), pygame.RESIZABLE
-)
-
 
 class CatFacts:
     def __init__(self):
+        pygame.init()
         self.getBreedInfo()
+        self.displayInfo = pygame.display.Info()
+        self.screen = pygame.display.set_mode(
+            (self.displayInfo.current_w, self.displayInfo.current_h), pygame.RESIZABLE
+        )
+        self.sysFont = pygame.font.SysFont("segoeuisymbol", 20)
+        self.font = pygame.font.Font("./NotoEmoji-Medium.ttf", 20)
 
     def getBreedInfo(self):
         req = urllib.request.Request("https://api.thecatapi.com/v1/breeds")
@@ -31,14 +32,33 @@ class CatFacts:
                 images[key] = value["image"]["url"]
         self.images = images
 
-    def showPic(self, name, image):
+    def showPic(self, name, image, origin, description):
         pygame.display.set_caption(name)
         req = urllib.request.Request(image, headers={"User-Agent": "Mozilla/5.0"})
         res = urllib.request.urlopen(req)
         surface = pygame.image.load(BytesIO(res.read())).convert()
         picture = pygame.transform.scale(surface, surface.get_rect().center)
-        screen.blit(picture, (0, 0))
+        self.screen.fill((0, 0, 0))
+        self.screen.blit(picture, (0, 0))
+        self.writeInfo(origin, description)
         pygame.display.flip()
+
+    def writeInfo(self, origin, description):
+        startingX = 20
+        startingY = 20
+        globe = self.font.render("\U0001F431", True, (255, 165, 0))
+        _, _, globeWidth, globeHeight = globe.get_rect()
+        originT = self.sysFont.render(origin, True, (255, 165, 0))
+        _, _, textWidth, _ = originT.get_rect()
+        descriptionT = self.sysFont.render(description, True, (255, 165, 0))
+        self.screen.blits(
+            [
+                (globe, (startingX, startingY)),
+                (originT, (startingX + globeWidth, startingY)),
+                (globe, (startingX + globeWidth + textWidth, startingY)),
+                (descriptionT, (startingX, startingY + globeHeight)),
+            ]
+        )
 
     def showAllPics(self):
         print(
@@ -46,11 +66,13 @@ class CatFacts:
         )
         time.sleep(3)
         for key, value in self.images.items():
-            self.showPic(key, value)
-            breed = f"\U0001F431 {key} \U0001F431"
-            origin = f"\U0001F30E {self.breedInfo[key]['origin']} \U0001F30F:"
-            description = f"{self.breedInfo[key]['description']}\n"
-            print(" ".join([breed, origin, description]))
+            origin = self.breedInfo[key]["origin"]
+            description = self.breedInfo[key]["description"]
+            breedLog = f"\U0001F431 {key} \U0001F431"
+            originLog = f"\U0001F30E {origin} \U0001F30F:"
+            descriptionLog = f"{self.breedInfo[key]['description']}\n"
+            self.showPic(key, value, origin, description)
+            print(" ".join([breedLog, originLog, descriptionLog]))
             time.sleep(10)
 
 
