8a5512860d9be99577ef005e2aba05ffd4455e50
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2023-05-05 20:41:02 -0700

    feat(big): restructure data, break long lines, use a Thread

--- main.py
+++ main.py
@@ -1,80 +1,145 @@
-import pygame
 import os
-import urllib.request
-import json as pyjs
-from io import BytesIO
 import time
+from pygame import font, init, display, mixer, image, transform, RESIZABLE
+from io import BytesIO
+from json import loads
+from operator import itemgetter
+from threading import Thread, Event
+from urllib.request import Request, urlopen
 
 
-class CatFacts:
-    def __init__(self):
-        pygame.init()
-        self.getBreedInfo()
-        self.displayInfo = pygame.display.Info()
-        self.screen = pygame.display.set_mode(
-            (self.displayInfo.current_w, self.displayInfo.current_h), pygame.RESIZABLE
-        )
-        self.sysFont = pygame.font.SysFont("segoeuisymbol", 20)
-        self.font = pygame.font.Font("./NotoEmoji-Medium.ttf", 20)
+class CatFacts(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.event = event
+        self.catEmoji = "\U0001F431"
+        self.audoFile = "./appandbanan.mp3"
 
-    def getBreedInfo(self):
-        req = urllib.request.Request("https://api.thecatapi.com/v1/breeds")
+    def killThread(self):
+        self.event.set()
+
+    def fetchCatFacts(self):
+        req = Request("https://api.thecatapi.com/v1/breeds")
         req.add_header("x-api-key", os.getenv("API_KEY"))
-        res = urllib.request.urlopen(req)
-        json = pyjs.loads(res.read().decode("utf-8"))
-        self.breedInfo = {breed["name"]: breed for breed in json}
-        self.buildImagesDict()
+        res = urlopen(req)
+        json = loads(res.read().decode("utf-8"))
+        pertinentFacts = []
+        for ele in json:
+            if not "image" in ele:
+                continue
+            image, name, description, origin = itemgetter(
+                "image", "name", "description", "origin"
+            )(ele)
+            imgB = self.fetchCatImg(image["url"])
+            pertinentFacts.append(
+                {
+                    "breed": name,
+                    "description": description,
+                    "origin": origin,
+                    "image": {
+                        "b": imgB,
+                    },
+                }
+            )
+        self.catFacts = pertinentFacts
+        return
 
-    def buildImagesDict(self):
-        images = {}
-        for key, value in self.breedInfo.items():
-            if "image" in value:
-                images[key] = value["image"]["url"]
-        self.images = images
+    def fetchCatImg(self, url):
+        req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+        res = urlopen(req)
+        imgB = BytesIO(res.read())
+        return imgB
 
-    def showPic(self, name, image, origin, description):
-        pygame.display.set_caption(name)
-        req = urllib.request.Request(image, headers={"User-Agent": "Mozilla/5.0"})
-        res = urllib.request.urlopen(req)
-        surface = pygame.image.load(BytesIO(res.read())).convert()
-        picture = pygame.transform.scale(surface, surface.get_rect().center)
+    def flipDisplay(self, breed, desc, origin, img):
+        imgB = img["b"]
+        self.display.set_caption(breed)
+        imgSurf = image.load(imgB).convert()
+        imgSurfT = transform.scale(imgSurf, imgSurf.get_rect().center)
         self.screen.fill((0, 0, 0))
-        self.screen.blit(picture, (0, 0))
-        self.writeInfo(origin, description)
-        pygame.display.flip()
+        self.screen.blit(imgSurfT, (0, 0))
+        self.prepareTextBlit(origin, desc)
+        self.display.flip()
 
-    def writeInfo(self, origin, description):
-        startingX = 20
-        startingY = 20
-        globe = self.font.render("\U0001F431", True, (255, 165, 0))
-        _, _, globeWidth, globeHeight = globe.get_rect()
-        originT = self.sysFont.render(origin, True, (255, 165, 0))
-        _, _, textWidth, _ = originT.get_rect()
-        descriptionT = self.sysFont.render(description, True, (255, 165, 0))
+    def prepareTextBlit(self, origin, desc):
+        emojiSurf = self.emojiFont.render(self.catEmoji, True, (251, 206, 177))
+        originSurf = self.textFont.render(origin, True, (137, 207, 240))
+        self.blitText(emojiSurf, originSurf, desc)
+
+    def blitText(self, emoji, origin, desc):
+        startX, startY = 20, 20
+        *_, emojiW, emojiH = emoji.get_rect()
+        *_, originW, _ = origin.get_rect()
         self.screen.blits(
             [
-                (globe, (startingX, startingY)),
-                (originT, (startingX + globeWidth, startingY)),
-                (globe, (startingX + globeWidth + textWidth, startingY)),
-                (descriptionT, (startingX, startingY + globeHeight)),
+                (emoji, (startX, startY)),
+                (origin, (startX + emojiW, startY)),
+                (emoji, (startX + emojiW + originW, startY)),
             ]
         )
+        self.utilBreakLines(desc, sum([emojiH + startY]))
 
-    def showAllPics(self):
-        print(
-            "\n \U0001F389 Welcome to the slideshow! Let's get this party started... \n"
+    def utilBreakLines(self, desc, prevH):
+        lines = []
+        line = ""
+        for word in desc.split():
+            if self.textFont.size(line + word)[0] < 400:
+                line += f"{word} "
+            else:
+                lines.append(line)
+                line = f"{word} "
+        lines.append(line)
+        self.utilBlitLines(lines, prevH)
+
+    def utilBlitLines(self, lines, prevH):
+        currH = prevH + 5
+        for line in lines:
+            lineToRender = self.textFont.render(line, True, (255, 165, 0))
+            self.screen.blit(lineToRender, (20, currH))
+            currH += sum([lineToRender.get_height(), 5])
+
+    def logToConsole(self, breed, desc, origin, _):
+        breedLog = f"\U0001F431 {breed} \U0001F431"
+        originLog = f"\U0001F30E {origin} \U0001F30F:"
+        descLog = f"{desc}\n"
+        print(" ".join([breedLog, originLog, descLog]))
+
+    def initSoundTrack(self):
+        mixer.init()
+        mixer.music.load(self.audoFile)
+        mixer.music.set_volume(0.7)
+        mixer.music.play()
+
+    def initPygame(self):
+        init()
+        self.display = display
+        self.screen = self.display.set_mode(
+            (display.Info().current_w, display.Info().current_h), RESIZABLE
         )
-        time.sleep(3)
-        for key, value in self.images.items():
-            origin = self.breedInfo[key]["origin"]
-            description = self.breedInfo[key]["description"]
-            breedLog = f"\U0001F431 {key} \U0001F431"
-            originLog = f"\U0001F30E {origin} \U0001F30F:"
-            descriptionLog = f"{self.breedInfo[key]['description']}\n"
-            self.showPic(key, value, origin, description)
-            print(" ".join([breedLog, originLog, descriptionLog]))
-            time.sleep(10)
+        self.emojiFont = font.Font("./NotoEmoji-Medium.ttf", 30)
+        self.textFont = font.SysFont("segoeuisymbol", 25)
+
+    def main(self):
+        while len(self.catFacts):
+            fact = [v for (k, v) in self.catFacts.pop(0).items()]
+            self.flipDisplay(*fact)
+            self.logToConsole(*fact)
+            self.event.wait(10)
+        print("Bye!")
+        self.killThread()
+
+    def run(self):
+        self.initPygame()
+        self.fetchCatFacts()
+        self.main()
 
 
-cf = CatFacts()
-cf.showAllPics()
+try:
+    print("Starting program.")
+    event = Event()
+    catFacts = CatFacts(event)
+    catFacts.daemon = True
+    catFacts.start()
+    while catFacts.event:
+        time.sleep(60)
+except (KeyboardInterrupt, SystemExit):
+    print("Received close signal. Stopping...")
